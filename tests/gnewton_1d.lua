62de59ab22136d83e3571712d3fe783f4ca525ee
Lesley De Cruz <lesley.decruz+git@gmail.com>
2015-12-30 21:16:57 +0100

    Add more tests for roots.

---
+++ tests/gnewton_1d.lua
@@ -0,0 +1,68 @@
+local roots = require("lgsl.roots")
+local printf = function(...) io.write(string.format(...)) end
+local rand, abs = math.random, math.abs
+math.randomseed(90357) -- reproducible
+local gnewton = roots.gnewton
+
+--------------------------------------------------------------------------------
+-- Example 1: polynomial (scalar)
+--------------------------------------------------------------------------------
+
+-- Determine the number from the roots table nearest to x, and its index
+local function getnearestroot(x, polyroots)
+  local dmin, imin = math.huge
+  for i,root in ipairs(polyroots) do
+    local d = abs(x-root)
+    if d < dmin then
+      imin = i
+      dmin = d
+    end
+  end
+  return polyroots[imin], imin
+end
+
+-- Generate a random polynomial of order nroots with coefficients no larger
+-- than maxval in absolute value.
+local function getpoly(nroots, maxval)
+  local polyroots = {}
+  for i=1,nroots do
+    polyroots[i] = 2 * maxval * (rand() - 0.5)
+  end
+  -- Polynomial
+  local f = function(x)
+    local result = 1
+    for i=1,nroots do
+      result = result * (x - polyroots[i])
+    end
+    return result
+  end
+  -- Derivative
+  local df = function(x)
+    local result = 0
+    for i=1,nroots do
+      local term = 1
+      for j=1,nroots do
+        if j~=i then
+          term = term * (x - polyroots[j])
+        end
+      end
+      result = result + term
+    end
+    return result
+  end
+  return f, df, polyroots
+end
+
+-- Note that if test_residual is used, epsabs should be defined >0 (epsrel is
+-- meaningless since the residual is compared to zero).
+local opts = {maxiter = 1000, epsabs = 1e-9, stop_fn = roots.test_residual}
+
+-- Find roots of generated polynomials.
+for i=1,20 do
+  local f,df,polyroots = getpoly(5,10)
+  local result = gnewton(f,df,0,opts)
+  local exact = getnearestroot(result.x, polyroots)
+  printf("polynom:    iter = %3u\tx = %.3f\t\terror = %.3e\tdx = %.3e\n",
+  result.iter,result.x,result.x - exact, math.abs(result.dx))
+end
+
---
+++ tests/newton.lua
@@ -0,0 +1,55 @@
+local sqrt = math.sqrt
+local m = require("lgsl.matrix")
+local roots = require("lgsl.roots")
+local printf = function(...) io.write(string.format(...)) end
+local abs = math.abs
+local newton = roots.newton
+
+--------------------------------------------------------------------------------
+-- Example 1: sqrt(5) (vector with single element)
+--------------------------------------------------------------------------------
+
+local opts = {maxiter = 100, epsabs = 0, epsrel = 1e-3}
+
+local function f5(x)
+  return m.vec{x[1]^2 - 5}
+end
+
+local function df5(x) 
+  return m.vec{2*x[1]}
+end
+
+local xini = m.vec{5.0}
+
+-- Run root finder
+local res = newton(f5,df5,xini,opts)
+
+printf(
+  "sqrt(5):    iter = %3u\tx = %.3f\t\terror = %.3e\tdx = %.3e\n",
+  res.iter,res.x[1],res.x[1] - sqrt(5), res.dx:norm())
+
+--------------------------------------------------------------------------------
+-- Example 3: Rosenbrock
+--------------------------------------------------------------------------------
+
+local a,b = 1,10 -- fn parameters
+opts = {maxiter = 1000, epsabs = 1e-7, stop_fn = roots.test_residual}
+
+local function fR(x)
+  return m.vec{ a* (1 - x[1]),b*(x[2] - x[1]^2)}
+end
+
+local function dfR(x)
+  return m.def{{       -a, 0},
+               {-2*b*x[1], b}}
+end
+
+xini = m.vec{-10,-5}
+
+-- Run root finder
+res = newton(fR,dfR,xini,opts)
+
+local x = res.x
+printf(
+  "rosenbrock: iter = %3u\tx = (%.3f,%.3f)\tf(x) = (%.3e,%.3e)\t  dx = %.3e\n",
+  res.iter,x[1],x[2],res.f(x)[1],res.f(x)[2],res.dx:norm())
